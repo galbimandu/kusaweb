464d86287635b252243c0f86800c9f0819d11dce
MKIM525 <mkim525@wisc.edu>
2022-11-20 15:37:43 -0600

    adding navigation bar

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,3 +1,7 @@
 export { Button } from "./Atoms/Button";
 export { Input } from "./Atoms/Input";
 export { Avatar } from "./Atoms/Avatar";
+export { Dropdown } from "./Atoms/Dropdown";
+export { AvatarMember } from "./Atoms/AvatarMember";
+export { Item } from "./Atoms/Menu/Item";
+export { Menu } from "./Atoms/Menu/Menu";
